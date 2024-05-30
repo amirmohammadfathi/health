6d48abee38a8259294c7a05c2722785b8d89000c
amir-mohammad <amir.mohammad.fathi.2002@gmail>
2024-05-30 08:49:38 +0330

    FEAT: finish practices

--- users/practices.py
+++ users/practices.py
@@ -5,7 +5,7 @@ practice_1 = {
         "Bent-over press": "3 sets of 10-12 reps",
         "Plank": "3 sets of 10-12 reps"
     },
-    
+
     "day-2": {
         "Lunges": "3 sets of 10-12 reps",
         "Dumbbell Shoulder Press": "3 sets of 10-12 reps",
@@ -16,9 +16,8 @@ practice_1 = {
     "day-3": "Rest"
 }
 
-
 practice_2 = {
-    "day-1":{
+    "day-1": {
         "Cardio": "30 minutes",
         "Jumping jacs": "3 sets of 12-15 reps",
         "Burpees,": "3 sets of 12-15 reps",
@@ -26,7 +25,7 @@ practice_2 = {
         "Plank": "3 sets of 12-15 reps",
     },
 
-    "day-2":{
+    "day-2": {
         "HIIT (High-Intensity Interval Training)-Sprint Intervals": "20 seconds sprint, 40seconds rest, repeat for 15-20 minutes"
     },
 
@@ -34,7 +33,7 @@ practice_2 = {
 }
 
 practice_3 = {
-    "day-1":{
+    "day-1": {
         "Bench Press": "4 sets of 8-12 reps",
         "Dumbbell Flyes": "4 sets of 8-12 reps",
         "Tricep Dips": "4 sets of 8-12 reps",
@@ -44,7 +43,7 @@ practice_3 = {
         "Deadlifts": "4 sets of 8-12 reps",
         "Pull-ups": "4 sets of 8-12 reps",
         "Barbell Curls": "4 sets of 8-12 reps"
-    }, 
+    },
 
     "day-3": {
         "Squats": "4 sets of 8-12 reps",
@@ -86,23 +85,159 @@ practice_5 = {
     }
 }
 
+practice_6 = {
+    "day-1": {
+        "Running": "30 minutes",
+        "Cycling": "30 minutes",
+        "Swimming": "30 minutes",
+    },
+
+    'day-2': {
+        "Strength Training (Full body workout including Squats, Deadlifts, BenchPresses": "3 sets of 10-12 reps"
+    },
+
+    'day-3': {
+        "Interval Training (Combining bursts of high-intensity exercise with periods of rest)": "20 minutes"
+    },
+
+    'day-4': {
+        "Yoga or Pilates for Flexibility and Core Strength": "30 minutes"
+    },
+
+    'day-5': {
+        "Active Rest (Light walking or recreational activity)": "30-45 minutes"
+    }
+}
+
+practice_7 = {
+    "day-1": {
+        "Low-Impact Cardio (Walking, Elliptical Trainer)": "30 minutes"
+    },
+
+    "day-2": {
+        "Circuit Training (Bodyweight exercises like Squats, Push-ups, Lunges)": "3 sets of 10-12 reps"
+    },
+
+    "day-3": {
+        "Swimming or Aqua Aerobics for Joint Support": "30 minutes"
+    },
+
+    "day-4": {
+        "Rest or Gentle Stretching": "20-30 minutes"
+    },
+
+    "day-5": {
+        "Yoga for Stress Relief and Flexibility": "30-45 minutes"
+    },
+}
+
+practice_8 = {
+    "day-1": {
+        "Compound Strength Training (Focus on multi-joint movements like Squats,Deadlifts, Bench Presses)": "30 sets of 10-12 reps"
+    },
+    "day-2": {
+        "High-Calorie Cardio (Running, Cycling, or Rowing)": "20-30 minutes",
+    },
+    "day-3": {
+        "Rest and Recovery": "all day"
+    },
+    "day-4": {
+        "Resistance Band Exercises for Muscle Isolation": "3 sets of 10-12 reps"
+    },
+    "day-5": {
+        "Nutrient-Dense Cardio (Swimming or Hiking)": "30 minutes"
+    }
+}
+
+practice_9 = {
+    "day-1": {
+        "Low-Impact Exercise (Walking or Stationary Cycling)": "20-30 minutes"
+    },
+    "day-2": {
+        "Seated Strength Training with Resistance Bands or Light Weights": "2 sets of 10-12 reps"
+    },
+    "day-3": {
+        "Gentle Yoga or Tai Chi for Flexibility and Stress Relief": "30-45 minutes"
+    },
+    "day-4": {
+        "Rest and Meditation": "20 minutes"
+    },
+    "day-5": {
+        "Water Aerobics or Aquatic Therapy": "30 minutes"
+    }
+}
 
 
+practice_10 = {
+    "day-1": {
+        "Low-impact cardio(brisk walking, swimming)": "30 minutes",
+    },
+
+    "day-2": {
+        "Rest or light activity (yoga, stretching)": "30-45 minutes"
+    },
+
+    "day-3": {
+        "Full-body bodyweight strength training (squats, lunges, push-ups modified if needed), planks, rows)": "20-30 minutes"
+    },
+
+    "day-4": "Rest or Light activity",
+
+    "day-5": {
+        "Low-impact cardio": "30 minutes"
+    },
+
+    "day-6": "Rest",
+
+    "day-7": "Rest",
+}
+
+practice_11 = {
+    "day-1": {
+        "Cardio(running, biking, elliptical)": "30-45 minutes",
+    },
+
+    "day-2": {
+        "Lower body strength training (quats, deadlifts, lunges, calf raises)": "20-30 minutes"
+    },
+
+    "day-3": "Rest or Light activity",
 
 
+    "day-4": {
+        "Upper body strength training (push-ups, rows, overhead press, bicep curls, tricep extensions)": "30-45 minutes"
+    },
 
 
+    "day-5": {
+        "HIIT (High-Intensity Interval Training)": "20-30 minutes"
+    },
 
+    "day-6": {
+        "Core strength training (planks, crunches, leg raises)": "20-30 minutes"
+    },
 
+    "day-7": "Rest",
+}
 
+practice_12 = {
+    "day-1": {
+        "Split training (focus on upper body or lower body)": "45-60 minutes"
+    },
 
+    "day-2": {
+        "Cardio": "45-60 minutes"
+    },
 
+    "day-3": "Rest or active recovery (light cardio, yoga)",
 
+    "day-4": "Split training (opposite muscle group from Day 1)",
 
+    "day-5": "HIIT or plyometrics (explosive jump training)",
 
+    "day-6": "Core and full-body strength training",
 
-
-
-
+    "day-7": "Rest"
+}
 
 
